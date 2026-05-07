14ef4527806ca529092bb4d4b35b51ccfd3bfb9b
4c696c79 <lilyjulio2005@gmail.com>
2026-05-07 20:01:00 -0300

    feat:corrrecciones al ejercicio

--- EjPOO/ReparadoresCelulares/Main.java
+++ EjPOO/ReparadoresCelulares/Main.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 
 public class Main{
     public static void main(String[] args) {
-        ArrayList<Cliente> cli = new ArrayList<>();
-        ArrayList<Tecnico> tec = new ArrayList<>();
+        //ArrayList<Cliente> cli = new ArrayList<>();
+        //ArrayList<Tecnico> tec = new ArrayList<>();
+        //Esos dos ArrayList pueden hacerse por separado, pero podemos usar polimorfismo que podes usar la clase padre y poder almacenar cada usuario de forma separada :D
+        ArrayList<Usuario> user = new ArrayList<>();
         //Añadir tecnicos
-        tec.add(new Tecnico("Juan", "juanjuanito@gmail.com", "31231221fds", "Reparador"));
-        tec.add(new Tecnico("Pedro", "PedroPoderoso@hotmail.com", "Pepe", "Marketing"));
-        tec.add(new Tecnico("Pedro", "PedroPoderoso@hotmail.com", "Pepe", "Marketing"));
+        user.add(new Tecnico("Juan", "juanjuanito@gmail.com", "31231221fds", "Reparador"));
+        user.add(new Tecnico("Pedro", "PedroPoderoso@hotmail.com", "Pepe", "Marketing"));
+        user.add(new Tecnico("Pedro", "PedroPoderoso@hotmail.com", "Pepe", "Marketing"));
 
         //Añadir clientes
-        cli.add(new Cliente("Laura", "LauraMate@yahoo.com.uy", "Laura", 99986457));
-        cli.add(new Cliente("el panadero", "bullll@gmail.com", "null", 983423422));
+        user.add(new Cliente("Laura", "LauraMate@yahoo.com.uy", "Laura", 99986457));
+        user.add(new Cliente("el panadero", "bullll@gmail.com", "null", 983423422));
 
-        for (Cliente p : cli) {
+        for (Usuario p : user) {
             p.mostrarRol();
         }
-        for (Tecnico pp : tec) {
-            pp.mostrarRol();
-        }
-        System.out.println("\nLa cantidad de usuarios en total son de "+(cli.size() + tec.size()));
+        
+        System.out.println("\nLa cantidad de usuarios en total son de "+user.size());
 
 
 
